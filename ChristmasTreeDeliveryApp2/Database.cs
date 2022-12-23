931d2873c0d04faec7fd13952f7f6ac56153a5d4
Valentinas Kaminskas <valentinas.kaminskas@accenture.com>
2022-12-23 20:17:31 +0200

    Add initial code

---
+++ ChristmasTreeDeliveryApp2/Database.cs
@@ -0,0 +1,32 @@
+﻿namespace ChristmasTreeDeliveryApp2
+{
+    public class Database
+    {
+        public Database()
+        {
+
+        }
+
+        public void GetAll(PresentsType type)
+        {
+            switch (type)
+            {
+                case PresentsType.RedcedarTree:
+                    // 
+                    break;
+                default:
+                    throw new EntryPointNotFoundException();
+                    break;
+            }
+        }
+    }
+
+    public enum PresentsType
+    {
+        RedcedarTree = 0,
+        CedarTree = 1,
+        ConiferTree = 2,
+        CypressTree = 3,
+        FirTree = 4,
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # unclean-code-demo
+
+This app is example of unclean code app. This project is used as exercise source code to teach clean code in practice.
+
+# Project domain
+- this is background service working as command line app.
