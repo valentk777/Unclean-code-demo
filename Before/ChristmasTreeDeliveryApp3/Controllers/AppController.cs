6392ba2a2f182e184596012d2df3d058249a7308
Valentinas Kaminskas <valentinas.kaminskas@accenture.com>
2023-02-20 12:20:45 +0200

    final structure

---
+++ Before/ChristmasTreeDeliveryApp3/Controllers/AppController.cs
@@ -0,0 +1,123 @@
+using System.ComponentModel;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.Metrics;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using ChristmasTreeDeliveryApp3.Controllers;
+
+namespace ChristmasTreeDeliveryApp3.Controllers
+{
+    [ApiController]
+    public class AppController : ControllerBase
+    {
+        private readonly ILogger<AppController> _logger;
+
+        public AppController(ILogger<AppController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("GetAllTrees")]
+        public ActionResult<List<TreeObjectDtoData>> Get1()
+        {
+            var trees = new List<TreeObjectDtoData>();
+
+            foreach (var type in new List<PresentsType>()
+            {
+                PresentsType.RedcedarTree, PresentsType.CedarTree, PresentsType.ConiferTree, PresentsType.CypressTree, PresentsType.FirTree
+            })
+            {
+                var db = new Database();
+
+                foreach (var result in db.AllTrees(type))
+                {
+                    if (result != null)
+                    {
+                        trees.Add(result);
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+            }
+
+            return Ok(trees);
+        }
+
+        public class Get2Request
+        {
+            public int type { get; set; }
+        }
+
+        [HttpGet]
+        [Route("GetAllTreesByType")]
+        public ActionResult<List<TreeObjectDtoData>> Get2([FromQuery] Get2Request request)
+        {
+            var trees = new List<TreeObjectDtoData>();
+
+            foreach (var new_type in new List<PresentsType>()
+            {
+                PresentsType.RedcedarTree, PresentsType.CedarTree, PresentsType.ConiferTree, PresentsType.CypressTree, PresentsType.FirTree
+            })
+            {
+                var db = new Database();
+
+                foreach (var result in db.AllTrees(new_type))
+                {
+                    if (result != null)
+                    {
+                        if ((int)result.TreeType == request.type)
+                        {
+                            trees.Add(result);
+                        }
+                        else
+                        {
+                            _logger.LogError("nothing to log");
+                            continue;
+                        }
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+            }
+
+            return Ok(trees);
+        }
+        
+        [Route("AddOrderOfTree")]
+        [HttpPost]
+        public async Task<ActionResult> Add1([Microsoft.AspNetCore.Mvc.FromBody] TreeObjectDtoData data)
+        {
+            var db = new Database();
+            var result = db.SaveTree(data.TreeName, data.TreeType, data.TreeDeliveredTo);
+
+            if (result.Item1 == true)
+            {
+                return Ok();
+            }
+            else
+            {
+                throw new EntryPointNotFoundException("results is not okay");
+                return Conflict();
+            }
+        }
+    }
+
+    public class TreeObjectDtoData
+    {
+        public string TreeName { get; set; }
+
+        public PresentsType TreeType { get; set; }
+
+        public string TreeDeliveredTo { get; set; }
+
+        public DateTime TreeDeliveredDate { get; set; } = DateTime.UtcNow;
+    }
+}
+
---
+++ Before/ChristmasTreeDeliveryAppTests/DatabaseTests.cs
@@ -0,0 +1,100 @@
+using ChristmasTreeDeliveryApp3;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ChristmasTreeDeliveryAppTests
+{
+    public class DatabaseTests
+    {
+        [Fact]
+        public void GivenEmptyDatabase_WhenAllTreesPresentsTypeRedcedarTree_ThenEmptyReturn()
+        {
+            // Arrange
+            var db = new Database();
+            File.Delete("treeRecord.txt");
+            // Assert
+
+            // Act
+            var result = db.AllTrees(PresentsType.RedcedarTree);
+
+            Assert.True(result.Count == 0);
+
+        }
+
+        [Fact]
+        public void GivenEmptyDatabase_WhenAllTreesPresentsTypeCedarTree_ThenEmptyReturn()
+        {
+            // Arrange
+            var db = new Database();
+            File.Delete("treeRecord.txt");
+
+            // Act
+            var result = db.AllTrees(PresentsType.CedarTree);
+
+            // Assert
+            Assert.True(result.Count == 0);
+
+        }
+        [Fact]
+        public void GivenEmptyDatabase_WhenAllTreesPresentsTypeConiferTree_ThenEmptyReturn()
+        {
+            // Arrange
+            var db = new Database();
+            File.Delete("treeRecord.txt");
+
+            var result = db.AllTrees(PresentsType.ConiferTree);
+            Assert.True(result.Count == 0);
+
+        }
+
+        [Fact]
+        public void GivenEmptyDatabase_WhenAllTreesPresentsTypeCypressTree_ThenEmptyReturn()
+        {
+            // Arrange
+            var db = new Database();
+            File.Delete("treeRecord.txt");
+
+            // Act
+            var result = db.AllTrees(PresentsType.CypressTree);
+
+            // Assert
+            Assert.True(result.Count == 0);
+
+        }
+
+        [Fact]
+        public void GivenEmptyDatabase_WhenAllTreesPresentsTypeFirTree_ThenEmptyReturn()
+        {
+            // Arrange
+            // Act
+            // Assert
+
+            var db = new Database();
+            File.Delete("treeRecord.txt");
+
+            var result = db.AllTrees(PresentsType.FirTree);
+
+            Assert.True(result.Count == 0);
+
+        }
+
+        [Fact]
+        public void GivenEmptyDatabase_WhenSaveTree_ThenOneSaved()
+        {
+            // Arrange
+            var db = new Database();
+            // if you remove this test will fail second time
+            // Act
+
+            File.Delete("treeRecord.txt");
+
+            var result = db.SaveTree("test1", PresentsType.FirTree, "my");
+
+            // Assert
+            Assert.True(result.Item1);
+            Assert.NotNull(result.Item2);
+            Assert.Equal("test1", result.Item2.TreeName);
+            Assert.Equal(result.Item2.TreeDeliveredTo, "my");
+            Assert.Equal(result.Item2.TreeType, PresentsType.FirTree);
+        }
+    }
+}
